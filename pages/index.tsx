714d8619cb1d9db49bc97cad7a7134aa7eaec376
Renan Souza <renantrsouza@gmail.com>
2023-01-11 15:11:03 -0400

    add: Container div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-//import Image from "next/image";
+import { Blobs } from "../src/components/Blobs";
 
 export default function Home() {
   return (
     <>
-      <main className="main"></main>
+      <main className="main">
+        <Blobs.BlobOne />
+        <Blobs.BlobTwo />
+        <div className="container"></div>
+      </main>
     </>
   );
 }
