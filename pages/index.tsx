1e337fa373207a196d6ad93eb16b86ce6a87671f
Renan Souza <renantrsouza@gmail.com>
2023-02-15 14:43:03 -0400

    add: Import the header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import { Header } from "../src/components/Header";
+
 export default function Home() {
   return (
     <>
       <main className="main">
-        <div className="container"></div>
+        <div className="container">
+          <Header />
+        </div>
       </main>
     </>
   );
