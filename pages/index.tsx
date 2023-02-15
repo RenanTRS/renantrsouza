e6af9a444ba8f2bcf6253eafea6cd32f5a05e808
Renan Souza <renantrsouza@gmail.com>
2023-02-15 15:26:41 -0400

    style: fix reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import { Header } from "../src/components/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="body">
       <main className="main">
         <div className="container">
           <Header />
         </div>
       </main>
-    </>
+    </div>
   );
 }
