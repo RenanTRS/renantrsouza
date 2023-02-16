b8b3ff8e404eebe88de118f59b2ca2369ba78b49
Renan Souza <renantrsouza@gmail.com>
2023-02-16 16:37:39 -0400

    add: Style to tags from header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { List, XSquare } from "phosphor-react";
 import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
+import style from "./Header.module.scss";
 
 export function Header() {
   const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
@@ -11,13 +12,15 @@ export function Header() {
   };
 
   return (
-    <header>
-      <Image
-        src={logoImg}
-        alt="Logo do site, com o nome <RenanTRS />"
-        priority
-        quality={100}
-      />
+    <header className={style.header}>
+      <div className={style.header__mobile_container}>
+        <Image
+          src={logoImg}
+          alt="Logo do site, com o nome <RenanTRS />"
+          priority
+          quality={100}
+          className={style.header__img}
+        />
 
         <button className={style.header__toggle} onClick={handleShowMobileMenu}>
           {!isMenuActive ? (
@@ -27,11 +30,23 @@ export function Header() {
           )}
         </button>
       </div>
+
+      <nav
+        className={`${style.header__nav} ${
+          isMenuActive && style.header__nav_active
+        }`}
+      >
         <ul>
           <li>
             <a href="#">Home</a>
+          </li>
+          <li>
             <a href="#">Sobre mim</a>
+          </li>
+          <li>
             <a href="#">Meus projetos</a>
+          </li>
+          <li>
             <a href="#">Projetos de cursos</a>
           </li>
         </ul>
