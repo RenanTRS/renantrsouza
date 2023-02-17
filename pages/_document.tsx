891a029f31a929ea76c099eac876ea8e2ead8c98
Renan Souza <renantrsouza@gmail.com>
2023-02-17 12:14:36 -0400

    add: Bebas neue style font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
