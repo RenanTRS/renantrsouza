a952cda1d222b99cf7d711e446984e324176ee16
Renan Souza <renantrsouza@gmail.com>
2023-02-21 13:27:07 -0400

    add: Create Main component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../src/components/Header";
 import { Heading } from "../src/components/Heading";
+import { Main } from "../src/screens/Main";
 
 export default function Home() {
   return (
@@ -7,7 +8,9 @@ export default function Home() {
       <main className="main">
         <Header />
 
-        <div className="container"></div>
+        <div className="container">
+          <Main />
+        </div>
       </main>
     </div>
   );
---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { tecnologies } from "../../utils/tecnologies";
+
+export function Main() {
+  const response = [
+    "html",
+    "css",
+    "styled-components",
+    "sass",
+    "javascript",
+    "typescript",
+    "reactjs",
+    "vitejs",
+    "jest",
+    "testing-library",
+    "graphql",
+    "figma"
+  ];
+
+  const logos = tecnologies.filter((index) => response.includes(index.name));
+
+  return (
+    <section id="home" className="section">
+      {logos.map((logo) => {
+        return (
+          <Image
+            key={logo.name}
+            src={logo.source}
+            width={40}
+            height={40}
+            alt={logo.alt}
+            priority
+          />
+        );
+      })}
+    </section>
+  );
+}
