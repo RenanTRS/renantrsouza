9ba5b941c9366e12afbdd4c75d52528ac9d9e994
Renan Souza <renantrsouza@gmail.com>
2023-03-06 13:47:07 -0400

    refact: Import Avatar component in the hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,24 @@
-import { Avatar } from "../Avatar";
+import Link from "next/link";
+import Image from "next/image";
+import { Anchor } from "../Anchor";
+import { GitLogo } from "../GitLogo";
 import { Heading } from "../Heading";
+import { LinkedInLogo } from "../LinkedInLogo";
+import { WhatsAppLogo } from "../WhatsAppLogo";
 
 import style from "./Hero.module.scss";
+import avatarImg from "../../assets/avatar.png";
 
 export function Hero() {
   return (
     <div className={style.hero}>
-      <Avatar />
+      <div className={style.avatar}>
+        <Image
+          src={avatarImg}
+          alt="Uma foto artística do desenvolvedor."
+          priority
+        />
+      </div>
 
       <div className={style.title}>
         <Heading size="lg">Renan T. R. Souza</Heading>
@@ -14,6 +26,33 @@ export function Hero() {
         <Heading size="md" asChild>
           <h2>Front-end Developer</h2>
         </Heading>
+
+        <div className={style.links}>
+          <Anchor type="download" />
+          <Link
+            href={"https://web.whatsapp.com/send?phone=5592982362834"}
+            target="_blank"
+            aria-label="Link para o WhatsApp"
+          >
+            <WhatsAppLogo />
+          </Link>
+
+          <Link
+            href={"https://www.linkedin.com/in/renantrsouza/"}
+            target="_blank"
+            aria-label="Link para o LinkedIn"
+          >
+            <LinkedInLogo />
+          </Link>
+
+          <Link
+            href={"https://github.com/RenanTRS"}
+            target="_blank"
+            aria-label="Link para o GitHub"
+          >
+            <GitLogo />
+          </Link>
+        </div>
       </div>
     </div>
   );
