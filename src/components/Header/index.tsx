d2ee674477e2533e2c8cdbcbe21fc8f0d5ef7fde
Renan Souza <renantrsouza@gmail.com>
2023-03-06 13:40:35 -0400

    add: Link component and import hook useVibrate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { List, XSquare } from "phosphor-react";
 import { useState } from "react";
+import { Anchor } from "../Anchor";
 import { Logo } from "../Logo";
+import { useVibrate } from "../../hooks/useVibrate";
 import style from "./Header.module.scss";
 
 export function Header() {
@@ -8,42 +11,75 @@ export function Header() {
 
   const handleShowMobileMenu = () => {
     setIsMenuActive(!isMenuActive);
+    useVibrate(60);
   };
 
   return (
-    <header className={style.header}>
-      <div className={style.header__mobile_container}>
-        <Logo size="header" />
+    <>
+      <header className={style.header}>
+        <div className={style.mobile_header_container}>
+          <Logo size="header" />
 
-        <button
-          className={style.header__toggle}
-          onClick={handleShowMobileMenu}
-          aria-label="Mostrar menu"
+          <button
+            className={style.mobile_header_container__toggle_button}
+            onClick={handleShowMobileMenu}
+            aria-label="Mostrar menu"
+          >
+            {!isMenuActive ? (
+              <List size={32} weight="bold" />
+            ) : (
+              <XSquare size={32} weight="bold" />
+            )}
+          </button>
+        </div>
+
+        <nav
+          className={`${style.nav} ${isMenuActive ? style.nav__active : ""}`}
         >
-          {!isMenuActive ? (
-            <List size={32} weight="bold" />
-          ) : (
-            <XSquare size={32} weight="bold" />
-          )}
-        </button>
-      </div>
+          <ul className={style.nav__list}>
+            <li>
+              <Link
+                href="#"
+                className={style.nav__link}
+                onClick={() => useVibrate(45)}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className={style.nav__link}
+                onClick={() => useVibrate(45)}
+              >
+                Sobre mim
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className={style.nav__link}
+                onClick={() => useVibrate(45)}
+              >
+                Meus projetos
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className={style.nav__link}
+                onClick={() => useVibrate(45)}
+              >
+                Projetos de cursos
+              </Link>
+            </li>
+          </ul>
 
-      <nav className={`${style.nav} ${isMenuActive ? style.nav__active : ""}`}>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Sobre mim</a>
-          </li>
-          <li>
-            <a href="#">Meus projetos</a>
-          </li>
-          <li>
-            <a href="#">Projetos de cursos</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <div className={style.nav__download}>
+            <Anchor type="download" />
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
