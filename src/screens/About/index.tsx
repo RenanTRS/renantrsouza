91e2420e05aa4593dc9da277b2ae6653cd36cc0c
Renan Souza <renantrsouza@gmail.com>
2023-03-10 11:57:20 -0400

    Add: article tag  and lang in strongs

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -17,20 +17,26 @@ export function About() {
             <Tecnologies className={style.icon} />
           </div>
 
-          <Text>
-            Olá, sou um <strong>Front-end</strong> developer com foco em{" "}
-            <strong>ReactJs</strong> e <strong>TypeScript</strong> há mais de 01
-            ano, no presente momento estou estudando a construção de Single Page
-            Applications (SPA) utilizando o Framework <strong>NextJs</strong>,
-            também tenho planos para em breve expandir meus conhecimentos para
-            outras stacks como <strong>Back-end</strong> e{" "}
-            <strong>Mobile</strong>. Sou formado em Análise e Desenvolvimento de
-            Sistemas (ADS) pela Escola Superior Batista do Amazonas (ESBAM) e
-            apaixonado por tecnologia, atualmente ainda não possuo experiências
-            profissionais como um desenvolvedor, mas venho estudando e me
-            aprimorando cada vez mais para agregar valor, contribuir e aprender
-            com o time ou equipe de desenvolveores que eu estiver.
-          </Text>
+          <article>
+            <Text>
+              Olá, sou um <strong lang="en-us">Front-end Developer</strong> com
+              foco em <strong lang="en-US">React Js</strong> e{" "}
+              <strong lang="en-us">Typescript</strong> há mais de um ano, no
+              presente momento estou estudando a construção de{" "}
+              <span lang="en-us">Single Page Applications</span> (SPA)
+              utilizando o <span lang="en-us">Framework</span>{" "}
+              <strong lang="en-us">Next Js</strong>, também tenho planos para em
+              breve expandir meus conhecimentos para outras stacks como{" "}
+              <strong lang="en-us">Back-end</strong> e{" "}
+              <strong lang="en-us">Mobile</strong>. Sou formado em Análise e
+              Desenvolvimento de Sistemas (ADS) pela Escola Superior Batista do
+              Amazonas (ESBAM) e apaixonado por tecnologia, ainda não possuo
+              experiências profissionais como um desenvolvedor, mas venho
+              estudando e me aprimorando cada vez mais para agregar valor,
+              contribuir e aprender com o time ou equipe de desenvolvedores em
+              que eu estiver.
+            </Text>
+          </article>
         </div>
       </div>
 
