a10d7da394bef9add6cdd43774a0b62fb4cb0828
Renan Souza <renantrsouza@gmail.com>
2023-03-14 17:20:48 -0400

    add: import to styles from swiper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../src/styles/reset.scss";
 import "../src/styles/global.scss";
+import "../src/styles/swiper.scss";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
