96bde842b5b3131370b04ae3a469c63c6e1a7bcf
Renan Souza <renantrsouza@gmail.com>
2023-03-17 15:03:13 -0400

    refact: Add types in getMedia hook

--- src/hooks/useGetMedia.ts
+++ src/hooks/useGetMedia.ts
@@ -1,12 +1,58 @@
 import { useEffect, useRef, useState } from "react";
 
-export function useGetMedia(width: number) {
+type MediaProps =
+  | "small-screen"
+  | "mobile-sm"
+  | "mobile"
+  | "tablet"
+  | "tablet-lg"
+  | "desktop"
+  | "desktop-lg";
+
+const sizesOfScreen = [
+  {
+    name: "small-screen",
+    size: 282
+  },
+  {
+    name: "mobile-sm",
+    size: 283
+  },
+  {
+    name: "mobile",
+    size: 640
+  },
+  {
+    name: "tablet",
+    size: 768
+  },
+  {
+    name: "tablet-lg",
+    size: 1024
+  },
+  {
+    name: "desktop",
+    size: 1280
+  },
+  {
+    name: "desktop-lg",
+    size: 1536
+  }
+];
+
+export function useGetMedia(width: MediaProps = "small-screen") {
+  const size = sizesOfScreen.find((index) => index.name === width);
+
   const [toggleChange, setToggleChange] = useState<boolean>(false);
 
   const matchMediaRef = useRef<MediaQueryList | null>(null);
 
   useEffect(() => {
-    matchMediaRef.current = window.matchMedia(`(min-width: ${width}px)`);
+    if (width === "small-screen") {
+      matchMediaRef.current = window.matchMedia(`(max-width: ${size?.size}px)`);
+    } else {
+      matchMediaRef.current = window.matchMedia(`(min-width: ${size?.size}px)`);
+    }
 
     const initialMatchMedia = matchMediaRef.current.matches;
 
