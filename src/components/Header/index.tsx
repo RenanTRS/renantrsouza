187f03de577cc34de98d69f70f6c260427b2afcb
Renan Souza <renantrsouza@gmail.com>
2023-04-13 20:37:43 -0400

    refact: Fix the position of  the header in large screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,43 +5,52 @@ import { Anchor } from "../Anchor";
 import { Logo } from "../Logo";
 import { useVibrate } from "../../hooks/useVibrate";
 import style from "./Header.module.scss";
+import { NavigationMobile } from "../NavigationMobile";
+import { NavigationDesktop } from "../NavigationDesktop";
+import { useGetMedia } from "../../hooks/useGetMedia";
 
 export function Header() {
-  const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+  /* const [isMenuActive, setIsMenuActive] = useState<boolean>(false); */
 
-  const handleShowMobileMenu = () => {
+  /* const handleShowMobileMenu = () => {
     setIsMenuActive(!isMenuActive);
     useVibrate(60);
-  };
+  }; */
 
-  const handleRedirect = () => {
+  /* const handleRedirect = () => {
     setIsMenuActive(!isMenuActive);
     useVibrate(45);
-  };
+  }; */
+
+  const isLargeScreen = useGetMedia("desktop");
 
   return (
     <>
       <header className={style.header}>
-        <div className={style.mobile_header_container}>
-          <Logo size="header" aria-label="Logo do site." />
+        {!isLargeScreen && <NavigationMobile />}
+        {isLargeScreen && <NavigationDesktop />}
+        {/* <div className={style.main_container}>
+          <div className={style.mobile_header_container}>
+            <Logo size="header" aria-label="Logo do site." />
 
-          <button
-            className={style.mobile_header_container__toggle_button}
-            onClick={handleShowMobileMenu}
-            aria-label={isMenuActive ? "Fechar menu" : "Abrir menu"}
-            aria-controls="menu"
-            aria-haspopup="true"
-            aria-expanded={!isMenuActive ? "false" : "true"}
-          >
-            {!isMenuActive ? (
-              <List size={32} weight="bold" aria-disabled />
-            ) : (
-              <XSquare size={32} weight="bold" aria-disabled />
-            )}
-          </button>
-        </div>
+            <button
+              className={style.mobile_header_container__toggle_button}
+              onClick={handleShowMobileMenu}
+              aria-label={isMenuActive ? "Fechar menu" : "Abrir menu"}
+              aria-controls="menu"
+              aria-haspopup="true"
+              aria-expanded={!isMenuActive ? "false" : "true"}
+            >
+              {!isMenuActive ? (
+                <List size={32} weight="bold" aria-disabled />
+              ) : (
+                <XSquare size={32} weight="bold" aria-disabled />
+              )}
+            </button>
+          </div>
+ */}
 
-        <nav
+        {/* <nav
           className={`${style.nav} ${isMenuActive ? style.nav__active : ""}`}
           id="menu"
         >
@@ -87,7 +96,7 @@ export function Header() {
           <div className={style.nav__download}>
             <Anchor type="download" />
           </div>
-        </nav>
+        </nav> */}
       </header>
     </>
   );
