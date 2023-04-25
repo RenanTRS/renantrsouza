d72d2b94d54acab74eb589b884a4c6f159a3c3b4
Renan Souza <renantrsouza@gmail.com>
2023-04-25 11:01:19 -0400

    style: Add animation in the header of desktop comp

--- src/components/NavigationDesktop/index.tsx
+++ src/components/NavigationDesktop/index.tsx
@@ -12,22 +12,22 @@ export function NavigationDesktop() {
 
       <nav className={style.nav} id="menu">
         <ul className={style.nav__list} role="menubar">
-          <li role="menuitem">
+          <li role="menuitem" style={{ animationDelay: `${1 * 400}ms` }}>
             <Link href="#home" className={style.nav__link}>
               Home
             </Link>
           </li>
-          <li role="menuitem">
+          <li role="menuitem" style={{ animationDelay: `${2 * 400}ms` }}>
             <Link href="#about" className={style.nav__link}>
               Sobre mim
             </Link>
           </li>
-          <li role="menuitem">
+          <li role="menuitem" style={{ animationDelay: `${3 * 400}ms` }}>
             <Link href="#projects" className={style.nav__link}>
               Meus projetos
             </Link>
           </li>
-          <li role="menuitem">
+          <li role="menuitem" style={{ animationDelay: `${4 * 400}ms` }}>
             <Link href="#contact" className={style.nav__link}>
               Contatos
             </Link>
